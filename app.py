406b895d05bb0b0f6d997c3ff4a82588356a529b
fredeggs <ian.dauphin@gmail.com>
2022-07-18 17:41:50 -0400

    completed demo code

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+from crypt import methods
 from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from models import connect_db, db, User, Tweet
-from forms import UserForm, TweetForm
 from sqlalchemy.exc import IntegrityError
+from forms import UserForm, TweetForm
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///auth_demo"
@@ -13,6 +14,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 
 connect_db(app)
+db.create_all()
 
 toolbar = DebugToolbarExtension(app)
 
@@ -20,7 +22,6 @@ toolbar = DebugToolbarExtension(app)
 @app.route('/')
 def home_page():
     return render_template('index.html')
-
 
 @app.route('/tweets', methods=['GET', 'POST'])
 def show_tweets():
@@ -31,14 +32,12 @@ def show_tweets():
     all_tweets = Tweet.query.all()
     if form.validate_on_submit():
         text = form.text.data
-        new_tweet = Tweet(text=text, user_id=session['user_id'])
+        new_tweet = Tweet(text=text, user_id=session["user_id"])
         db.session.add(new_tweet)
         db.session.commit()
-        flash('Tweet Created!', 'success')
+        flash('Tweet Created!', "success")
         return redirect('/tweets')
-
-    return render_template("tweets.html", form=form, tweets=all_tweets)
-
+    return render_template("/tweets.html", tweets=all_tweets, form=form)
 
 @app.route('/tweets/<int:id>', methods=["POST"])
 def delete_tweet(id):
@@ -55,7 +54,6 @@ def delete_tweet(id):
     flash("You don't have permission to do that!", "danger")
     return redirect('/tweets')
 
-
 @app.route('/register', methods=['GET', 'POST'])
 def register_user():
     form = UserForm()
@@ -68,14 +66,12 @@ def register_user():
         try:
             db.session.commit()
         except IntegrityError:
-            form.username.errors.append('Username taken.  Please pick another')
+            form.username.errors.append('Username taken. Please pick another')
             return render_template('register.html', form=form)
-        session['user_id'] = new_user.id
+        session["user_id"] = new_user.id
         flash('Welcome! Successfully Created Your Account!', "success")
         return redirect('/tweets')
-
     return render_template('register.html', form=form)
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_user():
@@ -90,13 +86,12 @@ def login_user():
             session['user_id'] = user.id
             return redirect('/tweets')
         else:
-            form.username.errors = ['Invalid username/password.']
+            form.username.errors = ['Invalid username/password']
 
     return render_template('login.html', form=form)
-
 
 @app.route('/logout')
 def logout_user():
     session.pop('user_id')
-    flash("Goodbye!", "info")
+    flash("Goodbye!", "primary")
     return redirect('/')
--- models.py
+++ models.py
@@ -12,24 +12,23 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
 
-
 class Tweet(db.Model):
+
     __tablename__ = 'tweets'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     text = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-    user = db.relationship('User', backref="tweets")
-
+    user = db.relationship('User', backref='tweets')
 
 class User(db.Model):
 
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
-    username = db.Column(db.Text, nullable=False,  unique=True)
+    username = db.Column(db.Text, nullable=False, unique=True)
 
     password = db.Column(db.Text, nullable=False)
 
